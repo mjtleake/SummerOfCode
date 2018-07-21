acc52daebdc2dceab40f23ad45d6ab9b4cce151c
mjtleake <mjtleake@gmail.com>
2018-07-20 22:09:06 -0600

    Extend to nxn world.

--- soc01h-cc-maryjoy-leake.py
+++ soc01h-cc-maryjoy-leake.py
@@ -8,38 +8,36 @@ Created on Tue Jul 17 15:20:32 2018
 import random 
 import numpy as np
 
-def world_generator():
-    """Generates the continent where 1 refers to land and 0 refers to water.
-    
-    """    
+
+def world_generator(n):
+    #Generates the continent where 1 refers to land and 0 refers to water.  
     grid = []
-    for row in range(0,11):
+    for row in range(0,n):
         row = []
-        for i in range(0,11):
+        for i in range(0,n):
             x = random.choice([0, 1])
             row.append(x)
         grid.append(row)
     return grid
 
-grid = world_generator()
 
-def your_position():
+def your_position(n):
+    #Generates the coordinates of your initial position in your world. 
     grid_arr = np.array(grid)
-    #Generates your initial position in your world. 
-    r = random.randrange(0,11)
-    c = random.randrange(0,11)
+    r = random.randrange(0,n)
+    c = random.randrange(0,n)
     row, col = r, c
     if grid_arr[row][col] == 1:
         print(grid_arr)
         initial_position = (row, col)
-        print("You are standing at coordinates (row, col): ", initial_position)
+        print("You are standing at location (row, col): ", initial_position)
         return initial_position
     else:
-        your_position()
+        your_position(n)
 
-initial_position = your_position()
    
 def total_continent_count():
+    #Counts the total land continent. 
     count = 0
     for row in grid:
         for col in row:
@@ -47,5 +45,19 @@ def total_continent_count():
                 count += 1
     return count
             
-    
+
+def check_next_continent():
+    #Checks if the next tile of land is reachable
+    row = initial_position[0]
+    col = initial_position[1]
+    print(row)
+    #continent_taken = [initial_position]
+    grid_arr = np.array(grid)
+    #grid_arr[row][col] = 'X'
+    #print(grid_arr)
+
+world_size = 11
+grid = world_generator(world_size) 
+initial_position = your_position(world_size)
 print("Total continent count: ", total_continent_count())
+check_next_continent()
